ffa883e438aa3ef96dce3371c402c7d32d1d73b3
Max Taldykin <jorpic@gmail.com>
2017-08-28 20:41:28 +0300

    Remove unused Token interface

--- contracts/Goldmint.sol
+++ contracts/Goldmint.sol
@@ -19,50 +19,16 @@ contract SafeMath {
      }
 }
 
-// Standard token interface (ERC 20)
-// https://github.com/ethereum/EIPs/issues/20
-contract Token is SafeMath {
-     // Functions:
-     /// @return total amount of tokens
-     function totalSupply() constant returns (uint256 supply) {}
 
-     /// @param _owner The address from which the balance will be retrieved
-     /// @return The balance
-     function balanceOf(address _owner) constant returns (uint256 balance) {}
-
-     /// @notice send `_value` token to `_to` from `msg.sender`
-     /// @param _to The address of the recipient
-     /// @param _value The amount of token to be transferred
-     function transfer(address _to, uint256 _value) returns(bool) {}
-
-     /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-     /// @param _from The address of the sender
-     /// @param _to The address of the recipient
-     /// @param _value The amount of token to be transferred
-     /// @return Whether the transfer was successful or not
-     function transferFrom(address _from, address _to, uint256 _value)returns(bool){}
-
-     /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
-     /// @param _spender The address of the account able to transfer the tokens
-     /// @param _value The amount of wei to be approved for transfer
-     /// @return Whether the approval was successful or not
-     function approve(address _spender, uint256 _value) returns (bool success) {}
-
-     /// @param _owner The address of the account owning tokens
-     /// @param _spender The address of the account able to transfer the tokens
-     /// @return Amount of remaining tokens allowed to spent
-     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
-
-     // Events:
-     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
-contract StdToken is Token {
+contract StdToken is SafeMath {
      // Fields:
      mapping(address => uint256) balances;
      mapping (address => mapping (address => uint256)) allowed;
      uint public totalSupply = 0;
+
+     // Events:
+     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
      // Functions:
      function transfer(address _to, uint256 _value) returns(bool){
